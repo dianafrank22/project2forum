cae04eb4c8a8f9a019a85786ef93c95b6548356a
Diana Frank <diana.frank22@gmail.com>
2016-01-20 16:22:24 -0500

    edit to signup form

--- server.rb
+++ server.rb
@@ -24,7 +24,7 @@ module Forum
         table = "users"	
         username = params["username"]
         password = params["password"]
-        db.exec("INSERT INTO users (username, password) VALUES ('#{username}', '#{password}')")      
+        db.exec("INSERT INTO #{users} (username, password) VALUES ('#{username}', '#{password}')")      
   		  redirect ('/')
       end
 
