e468ad93c028649ea407fc9e84c99c6d90efbf4a
Diana Frank <diana.frank22@gmail.com>
2016-01-20 15:37:48 -0500

    creating tables in seeds file

--- lib/seeds.rb
+++ lib/seeds.rb
@@ -11,7 +11,19 @@ else
 conn = PG.connect(dbname: "project2")
 end 
 
+# creates table for categories
 
+conn.exec("DROP TABLE IF EXISTS categories")
+
+conn.exec("CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+   	number_of_posts NUM
+  )"
+)
+
+
+# creates user table
 conn.exec("DROP TABLE IF EXISTS users")
 
 conn.exec("CREATE TABLE users(
@@ -19,4 +31,33 @@ conn.exec("CREATE TABLE users(
 	username VARCHAR,
 	password VARCHAR
 	)"
+)
+
+
+# creates table for posts
+
+conn.exec("DROP TABLE IF EXISTS posts")
+
+conn.exec("CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    topic_name VARCHAR(255),
+   	category_id ,
+   	comments NUM ,
+   	votes NUM ,
+   	content VARCHAR,
+   	user_id
+  )"
+)
+
+
+# creates table for comments
+conn.exec("DROP TABLE IF EXISTS comments")
+
+conn.exec("CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+ 	content VARCHAR,
+ 	post_id 
+ 	user_id
+ 	votes NUM 
+  )"
 )
