1d378b6c9ec8c6f7485d9ce4f746edae38b68e06
Diana Frank <diana.frank22@gmail.com>
2016-01-20 12:31:33 -0500

    create database tables

--- lib/seeds.rb
+++ lib/seeds.rb
@@ -1,23 +1,63 @@
 require 'pg'
 
-# if ENV if ENV["RACK_ENV"] == 'production'
-# 	conn = PG.connect(
-# 		dbname: ENV["POSTGRES_DB"],
-# 		host: ENV["POSTGRES_HOST"],
-# 		password: ENV["POSTGRES_PASS"],
-# 		user: ENV["POSTGRES_USER"]
-# 		)
-# else
-# conn = PG.connect(dbname: "portfolio")
-# end
+if ENV if ENV["RACK_ENV"] == 'production'
+	conn = PG.connect(
+		dbname: ENV["POSTGRES_DB"],
+		host: ENV["POSTGRES_HOST"],
+		password: ENV["POSTGRES_PASS"],
+		user: ENV["POSTGRES_USER"]
+		)
+else
+conn = PG.connect(dbname: "project2")
+end
 
-# need to set 
+# creates table for categories
 
-# conn.exec("DROP TABLE IF EXISTS categories")
+conn.exec("DROP TABLE IF EXISTS categories")
 
-# conn.exec("CREATE TABLE categories(
-#     id SERIAL PRIMARY KEY,
-#     name VARCHAR(255),
-#    	number_of_posts VARCHAR
-#   )"
-# )
+conn.exec("CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+   	number_of_posts NUM
+  )"
+)
+
+
+# creates table for users
+conn.exec("DROP TABLE IF EXISTS users")
+
+conn.exec("CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    user_name VARCHAR (255),
+    password VARCHAR
+   )"
+)
+
+# creates table for posts
+
+conn.exec("DROP TABLE IF EXISTS posts")
+
+conn.exec("CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    topic_name VARCHAR(255),
+   	category_id ,
+   	comments NUM ,
+   	votes NUM ,
+   	content VARCHAR,
+   	user_id
+  )"
+)
+
+# creates table for comments
+conn.exec("DROP TABLE IF EXISTS comments")
+
+conn.exec("CREATE TABLE comments(
+    id SERIAL PRIMARY KEY,
+ 	content VARCHAR,
+ 	post_id 
+ 	user_id
+ 	votes NUM 
+  )"
+)
+
+# how to set that from another table 
