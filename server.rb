6bef07446938bb28634796085a789ae3769d3b12
Diana Frank <diana.frank22@gmail.com>
2016-01-20 13:06:45 -0500

    attempt at creating log in form

--- server.rb
+++ server.rb
@@ -1,15 +1,53 @@
 module Forum
 	class Server < Sinatra::Base
 
+ 	@@db = PG.connect(dbname: "project2")
 # homepage
 		get "/" do
 		erb :index
 		end
 
 # login page
-		# get "/login" do 
-		# erb :login
-		# end
+		get '/login' do
+   		erb :login
+  		end
+
+		post '/login' do
+	    	users = @@db.exec_params(<<-SQL, [params[:login_name]])
+	      		SELECT * FROM users WHERE login_name = $1
+	    	SQL
+				if @user	
+	    			if BCrypt::Password.new(@user["login_password_digest"]) == params[:login_password]
+	    			session["user_id"] = @user["id"]
+	    		erb :index
+	    	else 
+	    		@error = "Invalid Password"
+	    		erb :login
+	    	end 
+	  		else 
+	  			@error = "Invalid Username"
+	  			erb :login 
+	  		end
+  		end
+
+
+	# get "/signup" do 
+	# 	erb :signup
+	# end
+
+	# post "/signup" do 
+	# 	encrypted_password = BCrypt::Password.create(params[:login_password])
+	# 	users = @@db.exec_params(<<-SQL, [params[:login_name],encrypted_password]) 
+ #      INSERT INTO users (login_name, login_password_digest) VALUES ($1, $2) RETURNING id;
+ #      SQL
+
+	# 	session["user_id"] = users.first["id"]
+	#   erb :signup_success
+	# end
+
+
+
+
 
 		# get "/user/:id"
 		# params [:id]
