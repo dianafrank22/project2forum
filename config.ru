887769b182825e3adb74f01547de6657ff74120e
Diana Frank <diana.frank22@gmail.com>
2016-01-20 12:49:42 -0500

    made file run on rackup

--- config.ru
+++ config.ru
@@ -1,5 +1,8 @@
-require_relative "server"
+require "bundler/setup"
 require "pg"
 require "sinatra/base"
+require "sinatra/reloader"
+require "pry"
 
+require_relative "server"
 run Forum::Server
--- server.rb
+++ server.rb
@@ -7,42 +7,42 @@ module Forum
 		end
 
 # login page
-		get "/login" do 
-		erb :login
-		end
+		# get "/login" do 
+		# erb :login
+		# end
 
 		# get "/user/:id"
 		# params [:id]
 		# erb :user
 		# end
 # new post
-		get "/new"
-		erb :newpost
-		end
+# 		get "/new"
+# 		erb :newpost
+# 		end
 
-# viewing the post
-		get "/post/:id"
-		erb :post
-		end
+# # viewing the post
+# 		get "/post/:id"
+# 		erb :post
+# 		end
 
-# adding a comment
-		get "/comment"
-		erb :comment
-		end
+# # adding a comment
+# 		get "/comment"
+# 		erb :comment
+# 		end
 
-		# post "/comment" 
-		# # redirect? 
-		# end
+# 		# post "/comment" 
+# 		# # redirect? 
+# 		# end
 
-# view a list of categories
-		get "/categories"
-		erb :categories
-		end
+# # view a list of categories
+# 		get "/categories"
+# 		erb :categories
+# 		end
 
-# view a list of posts in that category
-		get "/category/:id"
-		erb :category
-		end 
+# # view a list of posts in that category
+# 		get "/category/:id"
+# 		erb :category
+# 		end 
 
 
 	end
