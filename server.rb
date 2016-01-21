fa5cbd6e727944d065affc1d11d06609ad9ec159
Diana Frank <diana.frank22@gmail.com>
2016-01-20 20:27:54 -0500

    changes to server

--- server.rb
+++ server.rb
@@ -1,3 +1,7 @@
+require "sinatra/base"
+require "pg"
+require "bcrypt"
+
 module Forum
 	class Server < Sinatra::Base
 
@@ -16,57 +20,64 @@ module Forum
   			redirect('/')
       end
 
+# signup page
   		get "/signup" do 
   			erb :signup
   		end
 
   		post "/signup" do 
-        table = "users"	
-        username = params["username"]
-        password = params["password"]
-        db.exec("INSERT INTO #{table} (username, password) VALUES ('#{username}', '#{password}')")      
-  		  redirect ('/')
+        encrypted_password = BCrypt::Password.create([:password])
+        username = params[:username]
+        newuser = db.exec_params("INSERT INTO users (username, encrypted_password) VALUES ($1, $2)");
+        erb :index
       end
 
+
+# user page
 		# get "/user/:id"
 		# params [:id]
 		# erb :user
 		# end
 
-      # new post
-      # get "/new"
-      # 	erb :newpost
-      # end
+# new post page
+      get "/new" do
+      	erb :newpost
+      end
 
-      # post "/new"
+      # post "/new" do
 
       #   redirects('/post/:id')
       # end
 
 # viewing the post
-	   # 	get "/post/:id"
+	   # 	get "/post/:id" do
     #     @id = db.exec_params("SELECT * FROM posts WHERE id = #{params["id"].to_i}").first
 	   #   	erb :post
 		  # end
 
 # # adding a comment
-# 		get "/comment"
-# 		erb :comment
-# 		end
+    #  get "/comment" do
+      # erb :comment
+    # end
 
-# 		# post "/comment" 
-# 		# # redirect? 
-# 		# end
+	   # post "/comment" do
+       # redirect? 
+    # end
 
-# # view a list of categories
-# 		get "/categories"
-# 		erb :categories
-# 		end
+# view a list of categories
+		get "/categories" do
+      @categories = db.exec("SELECT * FROM categories")
+		erb :categories
+		end
 
-# # view a list of posts in that category
-# 		get "/category/:id"
-# 		erb :category
-# 		end 
+# view a list of posts in that category
+		get "/category/:id" do
+      @category = db.exec("SELECT topic_name FROM posts WHERE category_id = #{params["id"].to_i}").first
+		erb :category
+		end 
+
+
+
     def db
       PG.connect(dbname: "project2")
     end
