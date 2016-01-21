0eddc2e00ab79fa3100ba14c41604bce1d2c0e44
Diana Frank <diana.frank22@gmail.com>
2016-01-21 13:09:20 -0500

    trying password encryption

--- server.rb
+++ server.rb
@@ -4,20 +4,8 @@ require "bcrypt"
 
 module Forum
 	class Server < Sinatra::Base
-    enable :sessions
 
 
-
-# checks to see if user is logged in
-      # def current_user 
-      #   if session["user_id"]
-      #     @user ||= @@db.exec_params(<<-SQL, [session["user_id"]]).first
-      #       SELECT * FROM users WHERE id= $1
-      #     SQL
-      #   else
-      #     {}
-      #   end
-      # end
 
  
 # homepage
@@ -30,18 +18,9 @@ module Forum
    			erb :login
   		end
 
-  # 	post "/login" do 
-  #      @user = @@db.exec_params("SELECT * FROM users WHERE username = $1", [params[:username]]).first
-  #        if @user 
-  #           if @user["password"] = params[:password]
-  #             redirect "/"
-  #           else
-  #             erb :login
-  #           end
-  #         else
-  #           erb :login
-  #         end
-  #     end
+  	post "/login" do 
+     erb :index
+      end
 
 
       # compare given information to database
@@ -82,39 +61,6 @@ module Forum
 
 
 
-# new post page
-      # get "/new" do
-      # 	erb :newpost
-      # end
-
-
-      # post "/new" do 
-      #   topic_name = params["topic_name"]
-      #   content = params["content"]
-      #   session["user_id"] = params["user_id"]
-
-
-
-      #  if ENV["RACK_ENV"] == 'production'
-      #     conn = PG.connect(
-      #     dbname: ENV["POSTGRES_DB"],
-      #     host: ENV["POSTGRES_HOST"],
-      #     password: ENV["POSTGRES_PASS"],
-      #     user: ENV["POSTGRES_USER"]
-      #     )
-      #   else
-      #    conn = PG.connect(dbname: "project2")
-      #   end
-
-      #   conn.exec_params( "INSERT INTO posts(topic_name, content, user_id) VALUES ($1, $2, $3)",
-      #   [topic_name, content, user_id]
-      #   )
-
-      #     # insert into statement user id from session 
-
-      #   @new_post = true
-      #   erb :post
-      #  end
 
 
 
