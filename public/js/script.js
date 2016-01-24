1e513658f094046dba8c9053e94e312b9d0e24e6
Diana Frank <diana.frank22@gmail.com>
2016-01-24 14:20:31 -0500

    css and jquery changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,15 @@
-// "use strict";
-// $(document).ready(function() {
+"use strict";
+$(document).ready(function() {
+
+$('.all_categories').each(function () {
+  var colors = ["#107fc3","#4b5760","#9a3896", "#82b9e3", "#17451c", "#601a36"];                
+  var rand = Math.floor(Math.random()*colors.length);           
+  $(this).css("background-color", colors[rand]);
+});	
 
 
-// $('#arrow').click(function () {
-//     $(this).addClass('voted');
-// });
+});
 
 
 
-// }); 
+
